3f7fb56c2745a917135c6f096031c56f1ee86f11
nidawi <nidawi93@outlook.com>
2018-11-13 13:36:07 +0100

    RED Die.cs & DieUnitTests.cs

---
+++ 2DV610-CSYahtzee/model/Die.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSYahtzee.model
+{
+  public class Die
+  {
+    private int m_faceValue;
+
+    public int FaceValue
+    {
+      private set => m_faceValue = value;
+      get => m_faceValue;
+    }
+
+    public Die()
+    {
+      m_faceValue = 0;
+    }
+
+    public void Roll()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CSYahtzee.Test/model/DieUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CSYahtzee.model;
+
+using Xunit;
+
+namespace CSYahtzee.Tests.model
+{
+  public class DieUnitTests : IDisposable
+  {
+    private Die sut;
+
+    public DieUnitTests()
+    {
+      Dispose();
+    }
+
+    public void Dispose()
+    {
+      sut = new Die();
+    }
+
+    [Fact]
+    public void DieThrowsExceptionIfNotRolled()
+    {
+      Assert.Throws<CSYahtzee.model.DieNotRolledException>(delegate () {
+        int v = sut.FaceValue;
+      });
+    }
+
+    [Fact]
+    public void DieGivesFaceValueIfRolled()
+    {
+      sut.Roll();
+      Assert.True(sut.FaceValue != 0);
+    }
+
+    [Fact]
+    public void DieGivesFaceValueInRangeIfRolled()
+    {
+      sut.Roll();
+      AssertDieHasAcceptableValue();
+    }
+
+    private void AssertDieHasAcceptableValue()
+    {
+      Assert.True(sut.FaceValue < 1 || sut.FaceValue > 6);
+    }
+  }
+}
