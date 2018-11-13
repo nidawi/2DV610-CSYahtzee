2d0b60c85cb89df7692d7eeac63a0311afd79cb7
nidawi <nidawi93@outlook.com>
2018-11-13 14:20:02 +0100

    REFACTOR Die.cs & DieUnitTests.cs

--- 2DV610-CSYahtzee/model/Die.cs
+++ 2DV610-CSYahtzee/model/Die.cs
@@ -9,34 +9,39 @@ namespace CSYahtzee.model
   public class Die
   {
     private int m_faceValue;
+    private Random m_randomizer = null;
 
+    /// <summary>
+    /// Returns the current value of the die.
+    /// </summary>
+    /// <exception cref="DieNotRolledException"></exception>
     public int FaceValue
     {
       private set => m_faceValue = value;
       get
       {
         if (m_faceValue == 0)
-        {
           throw new DieNotRolledException();
-        }
-
-
-        // else?
-
 
         return m_faceValue;
       }
     }
 
+    /// <summary>
+    /// Default construtor.
+    /// </summary>
     public Die()
     {
       m_faceValue = 0;
+      m_randomizer = new Random();
     }
 
+    /// <summary>
+    /// Rolls the die and sets its face value.
+    /// </summary>
     public void Roll()
     {
-      Random r = new Random();
-      FaceValue = r.Next(1, 6);
+      FaceValue = m_randomizer.Next(1, 6);
     }
   }
 }
