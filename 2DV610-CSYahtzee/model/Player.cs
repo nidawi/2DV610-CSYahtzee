d660e175bc7fd33b2064adde08e11ecc9eeda089
nidawi <nidawi93@outlook.com>
2018-11-16 18:00:02 +0100

    RED Player.cs & PlayerUnitTests.cs->ConstructorShouldNotThrowWithValidPlayername()

---
+++ 2DV610-CSYahtzee/model/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSYahtzee.model
+{
+  public class Player
+  {
+    public Player(string a_name)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CSYahtzee.Test/model/PlayerUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+using Moq;
+
+using CSYahtzee.model;
+
+namespace CSYahtzee.Tests.model
+{
+  public class PlayerUnitTests : IDisposable
+  {
+    private Player sut;
+    private string m_playerName = "DefaultName"; // default string is needed else constructor will throw.
+
+    public PlayerUnitTests()
+    {
+      Dispose();
+    }
+
+    public void Dispose()
+    {
+      sut = new Player(m_playerName);
+    }
+
+    [Fact]
+    public void ConstructorShouldNotThrowWithValidPlayername()
+    {
+      Assert.True(sut != null);
+    }
+  }
+}
