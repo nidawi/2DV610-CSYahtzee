3a3ef9b28c8f287e75ea1c429e04313d6fc95bc7
nidawi <nidawi93@outlook.com>
2018-11-16 18:15:09 +0100

    REFACTOR Player.cs & PlayerUnitTests.cs->ConstructorShouldNotThrowWithValidPlayername()

--- 2DV610-CSYahtzee/model/Player.cs
+++ 2DV610-CSYahtzee/model/Player.cs
@@ -10,9 +10,26 @@ namespace CSYahtzee.model
   {
     private string m_name;
 
+    public const int MIN_NAME_LENGTH = 2; // Imaginary business rule.
+
+    public string Name
+    {
+      get => throw new NotImplementedException();
+      protected set
+      {
+        if (value == null)
+          throw new NotImplementedException();
+
+        if (value.Length < MIN_NAME_LENGTH)
+          throw new NotImplementedException();
+
+        m_name = value;
+      }
+    }
+
     public Player(string a_name)
     {
-      m_name = a_name;
+      Name = a_name;
     }
   }
 }
