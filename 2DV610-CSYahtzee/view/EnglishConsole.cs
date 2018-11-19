7375aa334801c885a4e86645f3f7cf76b97cc8af
nidawi <nidawi93@outlook.com>
2018-11-19 14:54:39 +0100

    RED EnglishConsole.cs & EnglishConsoleUnitTests.cs->ShouldDisplayPlayernamePrompt()

---
+++ 2DV610-CSYahtzee/view/EnglishConsole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSYahtzee.view
+{
+  public class EnglishConsole : IConsole
+  {
+    public void DisplayPlayernamePrompt()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CSYahtzee.Test/view/EnglishConsoleUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using Xunit;
+using Moq;
+
+using CSYahtzee.view;
+
+namespace CSYahtzee.Tests.view
+{
+  public class EnglishConsoleUnitTests : IDisposable
+  {
+    private EnglishConsole sut;
+
+    private StringWriter m_testWriter;
+
+    public EnglishConsoleUnitTests()
+    {
+      Dispose();
+    }
+
+    public void Dispose()
+    {
+      sut = new EnglishConsole();
+
+      m_testWriter = new StringWriter();
+
+      Console.SetOut(m_testWriter);
+    }
+
+    [Fact]
+    public void ShouldDisplayPlayernamePrompt()
+    {
+      sut.DisplayPlayernamePrompt();
+      string expected = "Please provide a player name: ".Trim();
+      string actual = m_testWriter.ToString().Trim();
+
+      Assert.Equal(expected, actual);
+    }
+
+
+  }
+}
