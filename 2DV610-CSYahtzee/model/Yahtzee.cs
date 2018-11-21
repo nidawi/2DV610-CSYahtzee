cd02c3ace6275225e346a31dee4c3ac3bb9999c3
nidawi <nidawi93@outlook.com>
2018-11-21 13:59:28 +0100

    GREEN Yahtzee.cs & YahtzeeUnitTests.cs->ShouldAddAPlayer()

--- 2DV610-CSYahtzee/model/Yahtzee.cs
+++ 2DV610-CSYahtzee/model/Yahtzee.cs
@@ -13,10 +13,13 @@ namespace CSYahtzee.model
   {
     private List<IPlayer> m_players;
 
+    private rules.IPlayerFactory m_playerFactory;
+
     public int PlayerCount => m_players.Count;
 
     public Yahtzee(rules.IPlayerFactory a_playerFactory, rules.IYahtzeeGameRulesAbstractFactory a_ruleFactory)
     {
+      m_playerFactory = a_playerFactory;
       m_players = new List<IPlayer>();
 
       if (a_playerFactory == null || a_ruleFactory == null)
@@ -27,7 +30,7 @@ namespace CSYahtzee.model
 
     public void AddPlayer(string name)
     {
-      throw new NotImplementedException();
+      m_players.Add(m_playerFactory.Player);
     }
   }
 }
