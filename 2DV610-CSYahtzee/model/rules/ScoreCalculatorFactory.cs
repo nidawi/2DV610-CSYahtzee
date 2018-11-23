503d9a294ff7b132854e05be21ffdce73ccb603d
nidawi <nidawi93@outlook.com>
2018-11-23 12:37:54 +0100

    REFACTOR ScoreCalculatorFactory.cs & ScoreCalculatorFactoryUnitTests.cs->ShouldReturnACalculatorBasedOnEnum()

--- 2DV610-CSYahtzee/model/rules/ScoreCalculatorFactory.cs
+++ 2DV610-CSYahtzee/model/rules/ScoreCalculatorFactory.cs
@@ -10,6 +10,11 @@ namespace CSYahtzee.model.rules
 {
   public class ScoreCalculatorFactory : IScoreCalculatorFactory
   {
+    /// <summary>
+    /// Creates and returns a Score Calculator.
+    /// </summary>
+    /// <param name="a_scoreCategory">Which calculator the factory should manufacture. (Get it? It's a factory)</param>
+    /// <returns>a Score Calculator.</returns>
     public IScoreCalculator GetScoreCalculator(ScoreCategory a_scoreCategory)
     {
       if (a_scoreCategory == ScoreCategory.Aces)
