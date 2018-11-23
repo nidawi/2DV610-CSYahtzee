dc8203a510eb900186825804b6654c98d3a6f51c
nidawi <nidawi93@outlook.com>
2018-11-23 16:28:54 +0100

    REFACTOR CategoryScore.cs & CategoryScoreUnitTests.cs->ShouldReturnGivenScore()

--- 2DV610-CSYahtzee/model/CategoryScore.cs
+++ 2DV610-CSYahtzee/model/CategoryScore.cs
@@ -27,7 +27,7 @@ namespace CSYahtzee.model
     
     public int Score
     {
-      get => m_score.HasValue ? m_score.Value : throw new NotImplementedException();
+      get => m_score.HasValue ? m_score.Value : throw new NotImplementedException(); // Still need to test what happens if score isn't set.
       private set
       {
         if (value < 0)
