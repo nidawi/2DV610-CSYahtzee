9bed1a6331b66a786cb0807d88084df8ad6e2c53
nidawi <nidawi93@outlook.com>
2018-11-24 15:01:53 +0100

    REFACTOR Scoresheet.cs & ScoresheetUnitTests.cs->GetScoreShouldThrowWhenNotGivenAPlayer()

--- 2DV610-CSYahtzee/model/Scoresheet.cs
+++ 2DV610-CSYahtzee/model/Scoresheet.cs
@@ -29,26 +29,23 @@ namespace CSYahtzee.model
   public class Scoresheet : IScoresheet
   {
 
-
     public Scoresheet(rules.IScoreCalculatorFactory a_scoreFactory)
     {
       if (a_scoreFactory == null)
-        throw new ArgumentNullException();
-
-      
+        throw new ArgumentNullException();     
     }
 
     public void RegisterScore(IPlayer a_player, ScoreCategory a_scoreCatagory, List<int> a_faceValues)
     {
       if (a_player == null) 
         throw new ArgumentNullException();
-
-   
     }
 
     public CategoryScore GetScore(IPlayer a_player, ScoreCategory a_scoreCatagory)
     {
-      if (a_player == null) throw new ArgumentNullException();
+      if (a_player == null) 
+        throw new ArgumentNullException();
+
       throw new NotImplementedException();
     }
 
